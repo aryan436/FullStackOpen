7096a263153ccaa98ae6f983d218bbdfb4a86720
aryan436 <aryantomar436@gmail.com>
2026-06-28 12:59:18 +0530

    exercise 2.9 complete added a search function

--- part2/Phonebook/src/App.jsx
+++ part2/Phonebook/src/App.jsx
@@ -1,9 +1,15 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Aryan Tomar",phone:"7869323122" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ]);
   const [newName, setNewName] = useState("");
   const [newPhone, setNewPhone] = useState("");
+  const [filter, setFilter] = useState("");
   const handleAdd = (event) => {
     event.preventDefault();
     const exists = persons.some((person) => person.name === newName)
@@ -13,7 +19,8 @@ const App = () => {
     }
     const newPerson = {
       name: newName,
-      phone:newPhone
+      number: newPhone,
+      id:persons.length+1
     };
     setPersons(persons.concat(newPerson));
     setNewName("")
@@ -27,9 +34,18 @@ const App = () => {
     setNewPhone(event.target.value);
     console.log(event.target.value)
   }
+  const personsToShow = persons.filter((person) =>
+    person.name.toLowerCase().includes(filter.toLowerCase()),
+  );
+  const handleFilter = (event) => {
+    console.log(event.target.value)
+    setFilter(event.target.value)
+  }
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>filter shown with<input value={filter} onChange={handleFilter}/></div>
+      <h2>add a new</h2>
       <form onSubmit={handleAdd}>
         <div>
           name: <input value={newName} onChange={handleNewName} />
@@ -42,8 +58,12 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => {
-        return <p key={person.name}>{person.name} { person.phone}</p>;
+      {personsToShow.map((person) => {
+        return (
+          <p key={person.id}>
+            {person.name} {person.number}
+          </p>
+        );
       })}
     </div>
   );
