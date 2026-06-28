037233c5932fc8ffe0976051a3b4952dbdfa0f56
aryan436 <aryantomar436@gmail.com>
2026-06-28 09:58:29 +0530

    excercise 2.1 complete folder structure looks great

---
+++ part2/courseinfo/src/components/Course.jsx
@@ -0,0 +1,11 @@
+import Header from "./Header"
+import Content from "./Content";
+const Course = ({ course }) => {
+    console.log(course);
+    
+    return <>
+        <Header heading={course.name} />
+        <Content parts={course.parts}/>
+    </>
+}
+export default Course
---
+++ part2/courseinfo/src/components/Part.jsx
@@ -0,0 +1,5 @@
+const Part = ({ part }) => {
+    console.log(part);
+    return <><p >{part.name} {part.exercises}</p></>;
+};
+export default Part
